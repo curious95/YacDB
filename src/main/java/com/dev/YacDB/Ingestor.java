5c5cd59f7f2f6e184032d87f5e9f9c8ce4f06a81
curious95 <kamalpradhan95@outlook.com>
2019-03-29 14:52:48 +0000

    Added basic driver init code and tested with a URL.

--- src/main/java/com/dev/YacDB/Ingestor.java
+++ src/main/java/com/dev/YacDB/Ingestor.java
@@ -1,5 +1,24 @@
 package com.dev.YacDB;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Ingestor {
 
+	
+	public static void ingest() {
+		
+		String chromeDriverPath = "/Users/devx/Documents/devx/EC_Wspace/YacDB/res/chromedriver" ;  
+		System.setProperty("webdriver.chrome.driver", chromeDriverPath);  
+		ChromeOptions options = new ChromeOptions();  
+		options.addArguments("headless");
+		options.addArguments ("--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");  
+		WebDriver driver = new ChromeDriver(options);  
+		driver.get("https://www.superyachts.com/motor-yacht-9161/azzam-specification.htm");
+		
+	}
+	
+	
 }
+	
--- src/main/java/com/dev/YacDB/YchDBInitializer.java
+++ src/main/java/com/dev/YacDB/YchDBInitializer.java
@@ -2,4 +2,13 @@ package com.dev.YacDB;
 
 public class YchDBInitializer {
 
+	
+	public static void main(String args[]) {
+		
+		Ingestor ingestor = new Ingestor();
+		ingestor.ingest();
+		
+	}
+	
+	
 }
