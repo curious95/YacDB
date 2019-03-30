dae9eab00c2aab2ac6dc59dc327af6a2c1728cd6
curious95 <kamalpradhan95@outlook.com>
2019-03-30 21:21:29 +0000

    fixed url spec bug

--- src/main/java/com/dev/YacDB/SuperYac.java
+++ src/main/java/com/dev/YacDB/SuperYac.java
@@ -1,7 +1,18 @@
 package com.dev.YacDB;
 
+import java.util.HashSet;
+import java.util.logging.Logger;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class SuperYac {
 
+	
+	final static int range = 251; // Static number during the design phase
+	static HashSet<String> urlSet = new HashSet<String>();
 	
 	public static void startProcess() {
 	
@@ -11,11 +22,44 @@ public class SuperYac {
 		
 		
 		
+		// Looping through pages
+		// https://www.superyachts.com/directory/completed_yachts.htm?page=2
+		
+		for(int i = 1; i<=1; i++) {
+			
+			urlSet.clear();
+			String URL = "https://www.superyachts.com/directory/completed_yachts.htm?page="+range;
+			Document doc = Jsoup.parse(ingestor.ingest(URL));
+			
+			Elements listItems = doc.getElementById("completeBoats").select("a");
+			
+			for(Element item:listItems) {				
+				urlSet.add(item.attr("href"));
+			}
+			
+			for(String url:urlSet) {
+				System.out.println(url.replace(".htm", "-specification.htm"));
+				ychParser(Jsoup.parse(ingestor.ingest(url.replace(".htm", "-specification.htm"))));
+			}
+			
+			//System.out.println(doc.text());
+			
+		}
 		
 		ingestor.closeDriver(); // Driver instances closes here
 		
 		
 	}
+	
+	
+	public static void ychParser(Document doc) {
+		
+		//System.out.println("In YCH");
+		System.out.println(doc.getElementsByClass("specifications").select("td"));
+		
+	}
+	
+	
 	
 	
 }
