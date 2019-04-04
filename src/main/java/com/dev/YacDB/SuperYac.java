fa23b69fc5418b528321e93ec85db6697c819131
curious95 <kamalpradhan95@outlook.com>
2019-04-04 01:15:55 +0100

    standardised fields

--- src/main/java/com/dev/YacDB/SuperYac.java
+++ src/main/java/com/dev/YacDB/SuperYac.java
@@ -10,80 +10,71 @@ import org.jsoup.select.Elements;
 
 public class SuperYac {
 
-	
 	final static int range = 251; // Static number during the design phase
 	static HashSet<String> urlSet = new HashSet<String>();
-	
+
 	public static void startProcess() {
-	
+
 		Ingestor ingestor = new Ingestor();
 		ingestor.initDriver(); // Driver instance begins here
-		
-		
-		
-		
+
 		// Looping through pages
 		// https://www.superyachts.com/directory/completed_yachts.htm?page=2
-		
-		for(int i = 2; i<=2; i++) {
-			
+
+		for (int i = 2; i <= 2; i++) {
+
 			urlSet.clear();
-			String URL = "https://www.superyachts.com/directory/completed_yachts.htm?page="+range;
+			String URL = "https://www.superyachts.com/directory/completed_yachts.htm?page=" + range;
 			Document doc = Jsoup.parse(ingestor.ingest(URL));
-			
+
 			Elements listItems = doc.getElementById("completeBoats").select("a");
-			
-			for(Element item:listItems) {				
+
+			for (Element item : listItems) {
 				urlSet.add(item.attr("href"));
 			}
-			
-			for(String url:urlSet) {
-				//System.out.println(url.replace(".htm", "-specification.htm"));
-				ychParser(Jsoup.parse(ingestor.ingest("https://www.superyachts.com"+url.replace(".htm", "-specification.htm"))));
+
+			for (String url : urlSet) {
+				// System.out.println(url.replace(".htm", "-specification.htm"));
+				ychParser(Jsoup.parse(
+						ingestor.ingest("https://www.superyachts.com" + url.replace(".htm", "-specification.htm"))));
+				System.out.println("\n\n\n\n\n ");
 			}
-			
-			//System.out.println(doc.text());
-			
+
+			// System.out.println(doc.text());
+
 		}
-		
+
 		ingestor.closeDriver(); // Driver instances closes here
-		
-		
+
 	}
-	
-	
+
 	public static void ychParser(Document doc) {
-		
+
 		boolean keyVal = false;
-		String key="", val="";
-		//System.out.println("In YCH");
-		Elements listItems  = doc.getElementsByClass("specifications").select("td");
-		
-		for(Element item:listItems) {
-			
-			//System.out.println(item.text());
-			
-			if(keyVal) {
+		String key = "", val = "";
+		// System.out.println("In YCH");
+		Elements listItems = doc.getElementsByClass("specifications").select("td");
+
+		for (Element item : listItems) {
+
+			// System.out.println(item.text());
+
+			if (keyVal) {
 				val = item.text();
-				System.out.println("Key = "+key+"  Val = "+val);
-				key="";
-				val="";
-				keyVal=false;
-				
+				System.out.println("Key = " + key + "  Val = " + val);
+				key = "";
+				val = "";
+				keyVal = false;
+
 			}
-			
-			if(item.text().contains(":")) {
+
+			if (item.text().contains(":")) {
 				keyVal = true;
-				key = item.text();
+				key = item.text().replace(" (", "-").replace(")", "");
 			}
-			
-			
-			
+
 		}
-		
+
 	}
-	
-	
-	
-	
+
 }
