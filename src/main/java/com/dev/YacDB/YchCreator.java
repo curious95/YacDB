4c8a41d8efd96bf75628b46d226d3910b8cab4c8
curious95 <kamalpradhan95@outlook.com>
2019-04-04 01:34:29 +0100

    final keylist created and variables generated

--- src/main/java/com/dev/YacDB/YchCreator.java
+++ src/main/java/com/dev/YacDB/YchCreator.java
@@ -8,29 +8,12 @@ import com.google.gson.JsonObject;
  */
 public class YchCreator {
 
-	static String yachtName;
-	static String length;
-	static String beam;
-	static String draft;
+	
 
-//	static String Name,Type,Model,Sub Type,Builder,Naval Architect,Exterior Designers,Interior Designer,Year,Flag,MCA,Class,Hull NB,
-//	Hull Colour, Length Overall,Length at Waterline,Beam,Draft-min,Draft-max,Gross Tonnage,Guests,Cabins Total,Cabins,Crew,Hull Configuration,Hull Material,Superstructure,Deck Material,Decks NB,Quantity,Fuel Type,Manufacturer,Model,Power,Total Power,Propulsion,Max Speed,Cruising Speed,Range,Fuel Capacity,Water Capacity,Generator,Stabilizers,Thrusters,Amenities;
-
-	public static void setLength(String length) {
-		YchCreator.length = length;
-	}
-
-	public static void setBeam(String beam) {
-		YchCreator.beam = beam;
-	}
-
-	public static void setDraft(String draft) {
-		YchCreator.draft = draft;
-	}
-
-	public static void setYachtName(String yachtName) {
-		YchCreator.yachtName = yachtName;
-	}
+	static String name,type,yac_model,sub_type,builder,naval_architect,exterior_designers,interior_designer,year,flag,mca,class_,hull_nb,hull_colour,length_overall,length_at_waterline,beam,draft_min,draft_max,gross_tonnage,guests,cabins_total,cabins,crew,hull_configuration,hull_material,superstructure,deck_material,decks_nb,quantity,fuel_type,manufacturer,eng_model,power,total_power,propulsion,max_speed,cruising_speed,range,fuel_capacity,water_capacity,generator,stabilizers,thrusters,amenities;
+	
+	
+	
 
 	public static JsonObject getYchObj() {
 
