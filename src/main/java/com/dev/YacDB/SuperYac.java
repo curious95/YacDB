1cfa5a4c5621387e9ad81e96cbf3844b0abe3142
curious95 <kamalpradhan95@outlook.com>
2019-04-04 05:35:56 +0100

    added cases for all vars

--- src/main/java/com/dev/YacDB/SuperYac.java
+++ src/main/java/com/dev/YacDB/SuperYac.java
@@ -40,51 +40,179 @@ public class SuperYac {
 			}
 
 			for (String url : urlSet) {
-				
-				//Clearing the old object map
+
+				// Clearing the old object map
 				objMap.clear();
-				
+
 				// Yacht creator class instance
 				YchCreator ych = new YchCreator();
-				
+
 				// System.out.println(url.replace(".htm", "-specification.htm"));
 				ychParser(Jsoup.parse(
 						ingestor.ingest("https://www.superyachts.com" + url.replace(".htm", "-specification.htm"))));
-				
+
 				// System.out.println("\n\n\n\n\n ");
-				
+
 				String fileName = "";
 				for (Map.Entry<String, String> entry : objMap.entrySet()) {
-					//System.out.println(entry.getKey() + "/" + entry.getValue());
-					
-					
+					// System.out.println(entry.getKey() + "/" + entry.getValue());
+
 					// Switch case to initialize setters
-					switch(entry.getKey()) {
-					
+					switch (entry.getKey()) {
+
 					case "name":
-						//System.out.println(entry.getValue());
+						// System.out.println(entry.getValue());
 						fileName = entry.getValue();
 						ych.setName(entry.getValue());
 						break;
 					case "type":
 						ych.setType(entry.getValue());
 						break;
+					case "yac_model":
+						ych.setYac_model(entry.getValue());
+						break;
+					case "sub_type":
+						ych.setSub_type(entry.getValue());
+						break;
+					case "builder":
+						ych.setBuilder(entry.getValue());
+						break;
+					case "naval_architect":
+						ych.setNaval_architect(entry.getValue());
+						break;
+					case "exterior_designers":
+						ych.setExterior_designers(entry.getValue());
+						break;
+					case "interior_designer":
+						ych.setInterior_designer(entry.getValue());
+						break;
+					case "year":
+						ych.setYear(entry.getValue());
+						break;
+					case "flag":
+						ych.setFlag(entry.getValue());
+						break;
+					case "mca":
+						ych.setMca(entry.getValue());
+						break;
+					case "class_":
+						ych.setClass_(entry.getValue());
+						break;
+					case "hull_nb":
+						ych.setHull_nb(entry.getValue());
+						break;
+					case "hull_colour":
+						ych.setHull_colour(entry.getValue());
+						break;
+					case "length_overall":
+						ych.setLength_overall(entry.getValue());
+						break;
+					case "length_at_waterline":
+						ych.setLength_at_waterline(entry.getValue());
+						break;
+					case "beam":
+						ych.setBeam(entry.getValue());
+						break;
+					case "draft_min":
+						ych.setDraft_min(entry.getValue());
+						break;
+					case "draft_max":
+						ych.setDraft_max(entry.getValue());
+						break;
+					case "gross_tonnage":
+						ych.setGross_tonnage(entry.getValue());
+						break;
+					case "guests":
+						ych.setGuests(entry.getValue());
+						break;
+					case "cabins_total":
+						ych.setCabins_total(entry.getValue());
+						break;
+					case "cabins":
+						ych.setCabins(entry.getValue());
+						break;
+					case "crew":
+						ych.setCrew(entry.getValue());
+						break;
+					case "hull_configuration":
+						ych.setHull_configuration(entry.getValue());
+						break;
+					case "hull_material":
+						ych.setHull_material(entry.getValue());
+						break;
+					case "superstructure":
+						ych.setSuperstructure(entry.getValue());
+						break;
+					case "deck_material":
+						ych.setDeck_material(entry.getValue());
+						break;
+					case "decks_nb":
+						ych.setDecks_nb(entry.getValue());
+						break;
+					case "quantity":
+						ych.setQuantity(entry.getValue());
+						break;
+					case "fuel_type":
+						ych.setFuel_type(entry.getValue());
+						break;
+					case "manufacturer":
+						ych.setManufacturer(entry.getValue());
+						break;
+					case "eng_model":
+						ych.setEng_model(entry.getValue());
+						break;
+					case "power":
+						ych.setPower(entry.getValue());
+						break;
+					case "total_power":
+						ych.setTotal_power(entry.getValue());
+						break;
+					case "propulsion":
+						ych.setPropulsion(entry.getValue());
+						break;
+					case "max_speed":
+						ych.setMax_speed(entry.getValue());
+						break;
+					case "cruising_speed":
+						ych.setCruising_speed(entry.getValue());
+						break;
+					case "range":
+						ych.setRange(entry.getValue());
+						break;
+					case "fuel_capacity":
+						ych.setFuel_capacity(entry.getValue());
+						break;
+					case "water_capacity":
+						ych.setWater_capacity(entry.getValue());
+						break;
+					case "generator":
+						ych.setGenerator(entry.getValue());
+						break;
+					case "stabilizers":
+						ych.setStabilizers(entry.getValue());
+						break;
+					case "thrusters":
+						ych.setThrusters(entry.getValue());
+						break;
+					case "amenities":
+						ych.setAmenities(entry.getValue());
+						break;
 					}
-					
+
 				}
-				
-				//System.out.println(ych.getYchObj().toString());
-				
-				File file = new File("jsonfiles/"+fileName+".json");
+
+				// System.out.println(ych.getYchObj().toString());
+
+				File file = new File("jsonfiles/" + "supYac" + ".json");
 				try {
-					FileUtils.writeStringToFile(file, ych.getYchObj().toString());
+					FileUtils.writeStringToFile(file, ych.getYchObj().toString() + "\n", true);
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
-				break; //break for limiting iterations during testing
-			}	
+
+				// break; //break for limiting iterations during testing
+			}
 
 		}
 
--- src/main/java/com/dev/YacDB/YchCreator.java
+++ src/main/java/com/dev/YacDB/YchCreator.java
@@ -10,8 +10,12 @@ public class YchCreator {
 
 	
 
-	static String name,type,yac_model,sub_type,builder,naval_architect,exterior_designers,interior_designer,year,flag,mca,class_,hull_nb,hull_colour,length_overall,length_at_waterline,beam,draft_min,draft_max,gross_tonnage,guests,cabins_total,cabins,crew,hull_configuration,hull_material,superstructure,deck_material,decks_nb,quantity,fuel_type,manufacturer,eng_model,power,total_power,propulsion,max_speed,cruising_speed,range,fuel_capacity,water_capacity,generator,stabilizers,thrusters,amenities;
-	
+	static String name, type, yac_model, sub_type, builder, naval_architect, exterior_designers, interior_designer,
+			year, flag, mca, class_, hull_nb, hull_colour, length_overall, length_at_waterline, beam, draft_min,
+			draft_max, gross_tonnage, guests, cabins_total, cabins, crew, hull_configuration, hull_material,
+			superstructure, deck_material, decks_nb, quantity, fuel_type, manufacturer, eng_model, power, total_power,
+			propulsion, max_speed, cruising_speed, range, fuel_capacity, water_capacity, generator, stabilizers,
+			thrusters, amenities;	
 	
 	
 
