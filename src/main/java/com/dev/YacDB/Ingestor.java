1d84083d95f1adda4aa3700b2270d02709df8699
curious95 <kamalpradhan95@outlook.com>
2019-04-30 14:39:13 +0100

    driver closed

--- src/main/java/com/dev/YacDB/Ingestor.java
+++ src/main/java/com/dev/YacDB/Ingestor.java
@@ -13,7 +13,7 @@ public class Ingestor {
 		String chromeDriverPath = "/Users/devx/Documents/devx/EC_Wspace/YacDB/res/chromedriverr" ;  
 		System.setProperty("webdriver.chrome.driver", chromeDriverPath);  
 		ChromeOptions options = new ChromeOptions();  
-		//options.addArguments("headless");
+		options.addArguments("headless");
 		options.addArguments ("--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");  
 		driver = new ChromeDriver(options);  
 		
